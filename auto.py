36e87cf171a6bc6c7b915592f65fde3af69db142
Visinigiri Aditya <adityakumarverified@gmail.com>
2020-12-12 20:22:23 +0530

    auto

---
+++ auto.py
@@ -0,0 +1,3 @@
+import os
+vspath = "C:\\Users\\ADMIN\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+os.startfile(vspath)
---
+++ start.bat
@@ -0,0 +1,3 @@
+cd C:\Users\ADMIN\Desktop\Automated
+python auto.py
+exit
